e1665d159076f99d303fa73dee1840b01885103b
Tanuj Sharma <hellotanujsharma@gmail.com>
2026-07-02 22:28:14 +0530

    add: Count Number of Nice Subarrays -> Sliding window

---
+++ Sliding_Window_and_Two_Pointers/Count_Number_of_Nice_Subarrays.cpp
@@ -0,0 +1,54 @@
+/*
+
+1248. Count Number of Nice Subarrays
+
+Given an array of integers nums and an integer k. A continuous subarray is
+called nice if there are k odd numbers on it.
+
+Return the number of nice sub-arrays.
+
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+  int helper(vector<int> &nums, int k) {
+    if (k < 0)
+      return 0;
+
+    int n = nums.size();
+
+    int left = 0, right = 0;
+    int odds = 0;
+    int ans = 0;
+
+    for (right = 0; right < n; right++) {
+      odds += nums[right] % 2;
+
+      while (odds > k) {
+        odds -= nums[left] % 2;
+        left++;
+      }
+
+      ans += right - left + 1;
+    }
+
+    return ans;
+  }
+  int numberOfSubarrays(vector<int> &nums, int k) {
+    int n = nums.size();
+
+    return helper(nums, k) - helper(nums, k - 1);
+  }
+};
+
+int main() {
+
+  cout << endl;
+  return 0;
+}
